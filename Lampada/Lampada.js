484b360cfdcd4e469926af5c03a6bf810549e980
Majorfelps <michael.as.cruz@gmail.com>
2023-04-17 21:20:47 -0300

    first commit

---
+++ Lampada/Lampada.js
@@ -0,0 +1,11 @@
+var lamp = window.document.getElementById('Lampada')
+
+function Ligar(){
+    lamp.src = 'ligada.svg'
+}
+function Desligar(){
+    lamp.src = 'Desligada.svg'
+}
+function Quebrada(){
+    lamp.src = 'quebrada.svg'
+}
---
+++ Lampada/s.js
@@ -0,0 +1,8 @@
+let idade = 15
+if(idade > 15 && idade < 18 || idade > 70){
+    console.log("O voto é opcional!")
+}else if(idade < 16){
+    console.log("Você não pode votar!")
+} else{
+    console.log("Você precisa Votar!")
+}
