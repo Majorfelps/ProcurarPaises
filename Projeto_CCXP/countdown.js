ea0c3a9ff84f3030f1639a1abdfadc17c67627c8
Majorfelps <michael.as.cruz@gmail.com>
2023-04-20 13:11:12 -0300

    first commit

---
+++ Projeto_CCXP/countdown.js
@@ -0,0 +1,31 @@
+const dia = document.getElementById('dia')
+const hora = document.getElementById('hora')
+const minuto = document.getElementById('minuto')
+const segundo = document.getElementById('segundo')
+
+const lancamento = "01 dec 2023"
+
+function countDown(){
+    const dataLanc = new Date(lancamento)
+    const hoje = new Date()
+
+    const segTotal = (dataLanc - hoje)/1000;
+
+    const finalDias = Math.floor(segTotal / 60 / 60 / 24);
+    const finalHoras = Math.floor(segTotal / 60 / 60) % 24;
+    const finalMinutos = Math.floor(segTotal / 60) % 60;
+    const finalSegundos = Math.floor(segTotal) % 60;
+
+    dia.innerText = ` ${finalDias} D `
+    hora.innerText = ` ${finalHoras} H `
+    minuto.innerText = ` ${finalMinutos} M `
+    segundo.innerText =  `${finalSegundos} S `
+}
+
+function formatoTempo( tempo ){
+    return tempo < 10? `0${tempo}` : tempo;
+}
+
+countDown();
+
+setInterval(countDown, 1000)
